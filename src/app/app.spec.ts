cf0119a2336e1716d3880797f39f1f83b5fc09b3
Marcelo Cíceri <mciceri@educ.gov.ar>
2017-05-18 16:00:22 -0300

    update angular-cli and added some tests

---
+++ src/app/app.spec.ts
@@ -0,0 +1,59 @@
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { IonicModule, IonicApp, IonicErrorHandler } from 'ionic-angular';
+import { ErrorHandler } from '@angular/core';
+import { NatacionApp } from './app.component';
+import { LoginPage } from '../pages/login-page/login-page';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+ 
+let comp: NatacionApp;
+let fixture: ComponentFixture<NatacionApp>;
+ 
+describe('Component: Root Component', () => {
+ 
+    beforeEach(async(() => {
+ 
+        TestBed.configureTestingModule({
+ 
+            declarations: [NatacionApp],
+ 
+            providers: [
+                StatusBar,
+                SplashScreen,
+                {provide: ErrorHandler, useClass: IonicErrorHandler}
+            ],
+ 
+            imports: [
+                IonicModule.forRoot(NatacionApp)
+            ]
+ 
+        }).compileComponents();
+ 
+    }));
+ 
+    beforeEach(() => {
+ 
+        fixture = TestBed.createComponent(NatacionApp);
+        comp    = fixture.componentInstance;
+ 
+    });
+ 
+    afterEach(() => {
+        fixture.destroy();
+        comp = null;
+    });
+ 
+    it('is created', () => {
+ 
+        expect(fixture).toBeTruthy();
+        expect(comp).toBeTruthy();
+ 
+    });
+ 
+    it('initialises with a root page of Login when not authenticated', () => {
+        expect(comp['rootPage']).toBe(LoginPage);
+    });
+ 
+});
